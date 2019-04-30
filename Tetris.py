3a70acd9f276511174bdf644206ec5209926d804
Julian Coronado <juliancoronado@csu.fullerton.edu>
2019-04-29 20:40:42 -0700

    Added new message function
    Now window will show score and moves taken

--- Tetris.py
+++ Tetris.py
@@ -586,7 +586,7 @@ class TetrisApp(object):
 		self.board = new_board()
 		self.new_stone()
 
-	def right_msg(self, msg):
+	def right_msg(self, msg, off_y):
 		for i, line in enumerate(msg.splitlines()):
 			msg_image =  pygame.font.Font(
 				pygame.font.get_default_font(), 12).render(
@@ -596,7 +596,7 @@ class TetrisApp(object):
 			msgim_center_x = 300
 			msgim_center_y = 300
 		
-			self.screen.blit(msg_image, (300, 300))
+			self.screen.blit(msg_image, (300, 300 + off_y))
 	
 	def center_msg(self, msg):
 		for i, line in enumerate(msg.splitlines()):
@@ -676,6 +676,7 @@ class TetrisApp(object):
 	
 	def run(self):
 		global moves_taken
+		global score
 		key_actions = {
 			'ESCAPE':	self.quit,
 			'LEFT':		lambda:self.move(-1),
@@ -702,7 +703,8 @@ class TetrisApp(object):
 				else:
 					self.draw_matrix(self.board, (0,0))
 					self.draw_matrix(self.stone, (self.stone_x, self.stone_y))
-					self.right_msg("Moves Taken: " + str(moves_taken))
+					self.right_msg("Moves Taken: " + str(moves_taken), 0)
+					self.right_msg("Score: " + str(score), 15)
 			pygame.display.update()
 			
 			for event in pygame.event.get():
