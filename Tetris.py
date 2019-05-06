28ee01f6f092c82836b3a2a7dc92533ceefecf6a
Julian Coronado <juliancoronado@csu.fullerton.edu>
2019-05-05 18:02:32 -0700

    Updated (with classes)

--- Tetris.py
+++ Tetris.py
@@ -46,6 +46,12 @@ colors = [
 (0, 220, 220)
 ]
 
+current_shape = {
+	'x': 0,
+	'y': 0, 
+	'shape': None
+}
+
 tetris_shapes = [
 	[[1, 1, 1],
 	 [0, 1, 0]],
@@ -65,468 +71,150 @@ tetris_shapes = [
 	[[6, 6, 6, 6]],
 	
 	[[7, 7],
-	 [7, 7]]
-]
+	 [7, 7]]]
 
-random_seed = 1
-current_shape = {
-	'x': 0,
-	'y': 0, 
-	'shape': None
-}
-
-bag = []
+moves_taken = 0
+score = 0
+move_limit = 100
 bag_index = 0
+bag = []
+upcoming_shape = []
 
-# GAME VALUES
-score = 0
-speed = 500
-save_state = None
-round_state = None
-next_draw = True
-moves_taken = 0
-move_limit = 500
-move_algorithm = []
-inspect_move_selection = False
+class AI:
 
-# LOCALSTORAGE - probably won't be needed
-storage = None
-
-def storing(stringed_archive):
-	storage = { 'archive': stringed_archive }
-
-# GENETIC ALGO VALUES
-# number of genomes
-population = 50
-genomes = []
-current_genome = 0
-generation = 0
-
-archive = {
-	'population_size': 0,
-	'current_generation': 0,
-	'elites': [], # fittest genomes that we define
-	'genomes': [] # all genomes
-}
-
-mutation_rate = 0.05 # can be tuned to make it better
-mutation_step = 0.2  # interval to mutation rate
-
-def initialize_population():
-	archive['population_size'] = population
-	next_shape()
-	apply_shape()
-	create_initial_population()
-
-# SELECTION
-def evaluate_next_genome():
-	global current_genome
-	current_genome = current_genome + 1
-	if current_genome == len(genomes):
-		evolve()
-	# load_state(round_state)
-	moves_taken = 0
-	make_next_move()
-
-def create_initial_population():
+	population_size = 50
+	current_gen = 0
+	curr_genome = 0
+	elites = []
 	genomes = []
-	for i in range(0, population):
-		genome = {
-			'id': uniform(0, 1),
-			'rows_cleared': uniform(0, 1) - 0.5,
-			'height_weight': uniform(0, 1) - 0.5,
-			'height_cumulative': uniform(0, 1) - 0.5,
-			'height_relative': uniform(0, 1) - 0.5,
-			'holes': uniform(0, 1) - 0.5,
-			'roughness': uniform(0, 1) - 0.5
-		}
-		genomes.append(genome)
-	evaluate_next_genome()
 
-# CROSSOVER
-# new generation
-def evolve():
-	global generation
-	global current_genome
-	current_genome = 0
-	generation = generation + 1
-	genomes = sorted(g_nomes, key = lambda k: k['fitness'])
-	archive['elites'].append(clone(genomes[0]))
-	print("Elite's fitness:", genomes[0].fitness)
+	def __init__(self):
+		self.next_shape()
+		self.apply_shape()
+		self.create_init_pop()
 
-	while len(genomes) > (population / 2):
-		genomes.pop()
+	def create_init_pop(self):
+		# generates all the random values for each genome
+		for i in range(0, self.population_size):
+			genome = {
+				'id': uniform(0, 1) - 0.5,
+				'rows_cleared': uniform(0, 1) - 0.5,
+				'height_weight': uniform(0, 1) - 0.5,
+				'height_cumulative': uniform(0, 1) - 0.5,
+				'height_relative': uniform(0, 1) - 0.5,
+				'holes': uniform(0, 1) - 0.5,
+				'roughness': uniform(0, 1) - 0.5,
+				'fitness': -1
+			}
+			self.genomes.append(genome)
+		self.evaluate_next_genome()
+
+	def evaluate_next_genome(self):
+		global moves_taken
+		self.curr_genome = self.curr_genome + 1
+		if self.curr_genome == len(self.genomes):
+			self.evolve()
+		moves_taken = 0
+		self.make_next_move()
 	
-	total_fitness = 0
-	for i in range(0, len(genomes)):
-		total_fitness = total_fitness + genomes[i].fitness
+	def evolve(self):
+		# nothing yet
+		print("evolve() function has been called.")
 
-	children = []
-	children.append(genomes[0])
-	while len(children) < population:
-		children.append(make_child(get_random_genome(), get_random_genome()))
+	def make_next_move(self):
+		global moves_taken, move_limit, score
+		moves_taken = moves_taken + 1
+		if moves_taken > move_limit:
+			self.genomes[self.curr_genome]['fitness'] = score
+			self.evaluate_next_genome()
+		else:
+			#TODO
+			possible_moves = self.get_all_possible_moves()
 	
-	genomes = []
-	genomes = genomes + children
-	archive['genomes'] = clone(genomes)
-	acrhive['current_generation'] = clone(gen)
+	def get_all_possible_moves(self):
+		print("get_all_possible_moves() function has been called.")
+		possible_moves = []
+		possible_move_ratings = []
+		iterations = 0
 
-	storing(json.dumps(archive, separators=(',', ':')))
+		for rots in range(0, 4):
+			#oldX = []
+			for t in range(-5, 5):
+				iterations = iterations + 1
 
-# MUTATION
-def make_child(parent1, parent2):
-	# inherit from first parent or second parent randomly
-	child = {
-		'id': uniform(0, 1),
-		'rows_cleared': random_choice(parent1['rows_cleared'], parent2['rows_cleared']),
-		'height_weight': random_choice(parent1['height_weight'], parent2['height_weight']),
-		'height_cumulative': random_choice(parent1['height_cumulative'], parent2['height_cumulative']),
-		'height_relative': random_choice(parent1['height_relative'], parent2['height_relative']),
-		'holes': random_choice(parent1['holes'], parent2['holes']),
-		'roughness': random_choice(parent1['roughness'], parent2['roughness']),
-		'fitness': -1
-	}
+				for j in range(0, rots):
+					self.rotate_shape()
 
-	# mutate each parameter
-	if uniform(0, 1) < mutation_rate:
-		child['rows_cleared'] = child['rows_cleared'] + uniform(0, 1) * mutation_step * 2 - mutation_step
-	if uniform(0, 1) < mutation_rate:
-		child['height_weight'] = child['height_weight'] + uniform(0, 1) * mutation_step * 2 - mutation_step
-	if uniform(0, 1) < mutation_rate:
-		child['height_cumulative'] = child['height_cumulative'] + uniform(0, 1) * mutation_step * 2 - mutation_step
-	if uniform(0, 1) < mutation_rate:
-		child['height_relative'] = child['height_relative'] + uniform(0, 1) * mutation_step * 2 - mutation_step
-	if uniform(0, 1) < mutation_rate:
-		child['holes'] = child['holes'] + uniform(0, 1) * mutation_step * 2 - mutation_step
-	if uniform(0, 1) < mutation_rate:
-		child['roughness'] = child['roughness'] + uniform(0, 1) * mutation_step * 2 - mutation_step
+				if (t < 0):
+					for left in range(0, abs(t)):
+						self.move_left()
 
-	return child
-
-def get_random_genome():
-	return genomes[random_weight_num(0, len(genomes - 1))]
-
-def random_weight_num(min, max):
-	return math.floor(math.pow(uniform(0, 1), 2) * (max - min + 1) + min)
-
-def random_choice(one, two):
-	if round(uniform(0, 1)) == 0:
-		return clone(one)
-	else:
-		return clone(two)
-
-def make_next_move():
-	# global moves_taken
-	# moves_taken = moves_taken + 1
-	if moves_taken > move_limit:
-		genomes[current_genome]['fitness'] = clone(score)
-		evaluate_next_genome()
-	else:
-		global next_draw
-		old_draw = clone(next_draw)
-		next_draw = False
-		possible_moves = get_all_possible_moves()
-		next_shape()
-		
-		for i in range(0, possible_moves):
-			next_move = get_highest_rated_move(get_all_possible_moves())
-			possible_moves[i]['rating'] = possible_moves[i]['rating'] + next_move['rating']
-		
-		load_state(last_state)
-		move = get_highest_rated_move(possible_moves)
-		
-		for i in range(0, move['rotations']):
-			rotate_shape()
-
-		if move['translation'] < 0:
-			for lefts in range(0, math.fabs(move['translation'])):
-				move_left()
-		elif move['translation'] > 0:
-			for rights in range(0, move['translation']):
-				move_right()
-
-		if (inspect_move_selection):
-			move_algorithm = move['algorithm']
-		
-		next_draw = old_draw
-
-		output()
-
-		# update_score()
-	
-def get_all_possible_moves():
-	possible_moves = []
-	possible_move_ratings = []
-	iterations = 0
-	for rotations in range(0, 4):
-		oldX = []
-		for t in range(-5, 6):
-			iterations = iterations + 1
-
-			for j in range(0, rotations):
-				rotate_shape()
-			
-			if t < 0:
-				for left in range(0, abs(t)):
-					move_left()
-			elif t > 0:
-				for right in range(0, t):
-					move_right()
-			
-			# TODO what is OldX doing here????
-			if not contains(oldX, current_shape['x']):
-				move_down_results = move_down()
 				
-				while move_down_results['moved']:
-					move_down_results = move_down()
-				
-				algorithm = {
-					'rows_cleared': move_down_results['rows_cleared'],
-					'height_weight': 0.5, #math.pow(get_height(), 1.5),
-					'height_cumulative': 0.5, # get_cumulative_height(),
-					'height_relative': 0.5, #get_relative_height(),
-					'holes': get_holes(), # this getter not done TODO
-					'roughness': get_roughness()
-				}
-
-				rating = 0
-				rating = rating + algorithm['rows_cleared'] * genomes[current_genome]['rows_cleared']
-				rating = rating + algorithm['height_weight'] * genomes[current_genome]['height_weight']
-				rating = rating + algorithm['height_cumulative'] * genomes[current_genome]['height_cumulative']
-				rating = rating + algorithm['height_relative'] * genomes[current_genome]['height_relative']
-				rating = rating + algorithm['holes'] * genomes[current_genome]['holes']
-				rating = rating + algorithm['roughness'] * genomes[current_genome]['roughness']
-
-				if move_down_results['lose']:
-					rating = rating - 500
-
-				possible_moves.append({
-					'rotations': rotations,
-					'translation': t,
-					'rating': rating,
-					'algorithm': algorithm
-				})
-
-				oldX.append(current_shape['x'])
-
-def get_highest_rated_move(moves):
-	max_rating = -100000000
-	max_move = -1
-	ties = []
-
-	for i in range(0, len(moves)):
-		if moves[i]['rating'] > max_rating:
-			max_rating = moves[i]['rating']
-			max_move = i
-			ties = [i]
-		elif moves[i]['rating'] == max_rating:
-			ties.append(i)
 	
-	move = moves[ties[0]]
-	move['algorithm']['ties'] = len(ties)
-	return move
+	def rotate_shape(self):
+		current_shape['shape'] = self.rotate(current_shape['shape'], 1)
 
-def clone(stringed_object):
-	return json.dumps(archive, separators=(',', ':'))
+	def rotate(self, shape, n):
+		for t in range(0, n):
+			shape = self.transpose(shape)
+			for i in range(0, len(shape)):
+				shape[i] = shape[i][::-1]
+		return shape
 
-def contains(oldX, currentX):
-	# idx = len(oldX)
-	# while idx >= 0:
-	# 	if oldX[idx] == currentX:
-	# 		return True
-	# 	idx = idx - 1
-	return False
+	def transpose(self, s):
+		transposed_array = [[s[j][i] for j in range(len(s))] for i in range(len(s[0]))]
+		return transposed_array
 
-def update():
-	if current_genome != -1:
-		results = move_down()
-		if not results['moved']:
-			if results['lose']:
-				genomes[current_genome]['fitness'] = clone(score)
-				evaluate_next_genome()
-			else:
-				make_next_move()
-	else:
-		move_down()
-	output()
-	# update_score()
+	def remove_shape(self):
+		for row in range(0, len(current_shape['shape'])):
+			for col in range(0, len(current_shape['shape'][row])):
+				if (current_shape['shape'][row][col] != 0):
+					grid[current_shape['y'] + row][current_shape['x'] + col] = 0
 
-def move_down():
-	global score
-	result = {
-		'lose': False,
-		'moved': True,
-		'rows_cleared': 0
-	}
+	def next_shape(self):
+		global bag_index, current_shape, grid, bag
+		bag_index = bag_index + 1
+		if (len(bag) == 0 or bag_index == len(bag)):
+			self.generate_bag()
+		if (bag_index == (len(bag) - 1)):
+			current_shape['shape'] = random.choice(tetris_shapes)
+		else:
+			current_shape['shape'] = bag[bag_index + 1]
 
-	remove_shape()
-	current_shape['y'] = current_shape['y'] + 1
-	
-	if collides(grid, current_shape):
-		current_shape['y'] = current_shape['y'] - 1
-		apply_shape()
-		next_shape()
-		result['rows_cleared'] = clear_rows()
-		if collides(grid, current_shape):
-			result['lose'] = True
-			#reset_game()
-		result['moved'] = False
-	apply_shape()
-	# score = score + 1
-	# output()
-	return result
+	def generate_bag(self):
+		global bag, bag_index
+		contents = ""
+		for i in range(0, 7):
+			t = random.choice(tetris_shapes)
+			bag.append(t)
+		bag_index = 0
 
-def move_left():
-	remove_shape()
-	current_shape['x'] = current_shape['x'] - 1
-	if collides(grid, current_shape):
-		current_shape['x'] = current_shape['x'] + 1
-	apply_shape()
+	def apply_shape(self):
+		for row in range(0, len(current_shape['shape'])):
+			for col in range(0, len(current_shape['shape'][row])):
+				if (current_shape['shape'][row][col] != 0):
+					grid[current_shape['y'] + row][current_shape['x'] + col] = current_shape['shape'][row][col]
 
-def move_right():
-	remove_shape()
-	current_shape['x'] = current_shape['x'] + 1
-	if collides(grid, current_shape):
+	def move_left(self):
+		self.remove_shape()
 		current_shape['x'] = current_shape['x'] - 1
-	apply_shape()
+		if self.collides():
+			current_shape['x'] = current_shape['x'] + 1
+		self.apply_shape()
 
-def clear_rows():
-	clear_rows_list = []
-	for row in range(0, len(grid)):
-		empty_spaces = False
-		for col in range(0, len(grid[row])):
-			if grid[row][col] == 0:
-				empty_spaces = True
-		if not empty_spaces:
-			clear_rows_list.append(row)
-	
-	# score, can take out
-	if len(clear_rows_list) == 1:
-		score = score + 400
-	elif len(clear_rows_list) == 2:
-		score = score + 1000
-	elif len(clear_rows_list) == 3:
-		score = score + 3000
-	elif len(clear_rows_list) == 4:
-		score = score + 12000
-	
-	cleared_rows = clone(len(clear_rows_list))
+	def collides(self):
+		global grid, current_shape
+		for row in range(0, len(current_shape['shape']) - 1):
+			for col in range(0, len(current_shape['shape'][row]) - 1):
+				if (current_shape['shape'][row][col] != 0):
+					if (grid[current_shape['y'] + row] == None or grid[current_shape['y'] + row][current_shape['x'] + col] == None or grid[current_shape['y'] + row][current_shape['x'] + col] != 0):
+						return True
+		return False
 
-	for i in range(len(clear_rows_list) - 1, -1, -1):
-		grid.pop(clear_rows_list[i])
+########################################################################################################################
+########################################################################################################################
 
-	while len(grid) < 20:
-		grid.insert(0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-	return clear_rows_list
-
-def apply_shape():
-	for row in range(0, len(current_shape['shape'])):
-		for col in range(0, len(current_shape['shape'][row])):
-			if current_shape['shape'][row][col] != 0:
-				grid[current_shape['y'] + row][current_shape['x'] + col] = current_shape['shape'][row][col]
-
-def remove_shape():
-	for row in range(0, len(current_shape['shape'])):
-		for col in range(0, len(current_shape['shape'][row])):
-			if current_shape['shape'][row][col] != 0:
-				grid[current_shape['y'] + row][current_shape['x'] + col] = 0
-
-def next_shape():
-	global bag_index, current_shape, grid
-	bag_index = bag_index + 1
-	if len(bag) == 0 or bag_index == len(bag):
-		generate_bag()
-	if bag_index == (len(bag) - 1):
-		previous_seed = random_seed
-		upcoming_shape = random.choice(tetris_shapes)
-		random_seed = previous_seed
-	else:
-		upcoming_shape = bag[bag_index + 1]
-
-	current_shape['shape'] = bag[bag_index]
-	current_shape['x'] = math.floor(len(grid[0]) / 2) - math.ceil(len(current_shape) / 2)
-	current_shape['y'] = 0
-
-def generate_bag():
-	global bag
-	contents = ""
-	for i in range(0, 7):
-		tetronimo = random.choice(tetris_shapes)
-		# while(contents.index(tetronimo) != -1):
-		# 	tetronimo = random.choice(tetris_shapes)
-		bag.append(tetronimo)
-		# contents = contents + tetronimo[i]
-	bag_index = 0
-
-def reset_game():
-	score = 0
-	grid = [
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0]
-	]
-	moves_taken = 0
-	generate_bag()
-	next_shape()
-
-def collides(grid, tetronimos):
-	for row in range(0, len(tetronimos['shape']) - 1):
-		for col in range(0, len(tetronimos['shape'][row]) - 1):
-			if tetronimos['shape'][row][col] != 0:
-				if grid[tetronimos['y'] + row] == None or grid[tetronimos['y'] + row][tetronimos['x'] + col] == None or grid[tetronimos['y'] + row][tetronimos['x'] + col] != 0:
-					return True
-	return False
-
-def rotate(matrix, times):
-	for t in range(0, times):
-		matrix = transpose(matrix)
-		for i in range(0, len(matrix)):
-			matrix[i].reverse()
-	return matrix
-
-def transpose(array):
-	transposed_array = [[array[j][i] for j in range(len(array))] for i in range(len(array[0]))]
-	return transposed_array
-
-def get_holes():
-	remove_shape()
-	peaks = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
-	for row in range(0, len(grid)):
-		for col in range(0, len(grid[row])):
-			if (grid[row][col] != 0 and peaks[col] == 20):
-				peaks[col] = row
-
-	holes = 0
-	for x in range(0, len(grid) - 1):
-		for y in range(0, len(peaks) - 1):
-			if (grid[x][y] == 0):
-				holes = holes + 1
-
-	apply_shape()
-	return holes
-
-moves_taken = 0
-score = 0
-
-class TetrisApp(object):
+class Tetris:
 	def __init__(self):
 		pygame.init()
 		pygame.key.set_repeat(250,25)
@@ -627,11 +315,7 @@ class TetrisApp(object):
 
 	def begin_ai(self):
 		self.ai = not self.ai
-		try:
-			#TODO this is where things begin to break
-			initialize_population()
-		except:
-			print("AI Toggle is broken.")
+		player = AI()
 
 	def remove_row(self, board, row):
 		global score
@@ -741,5 +425,6 @@ class TetrisApp(object):
 			clock.tick(config['maxfps'])
 
 if __name__ == '__main__':
-	App = TetrisApp()
-App.run()
+	game = Tetris()
+
+game.run()
